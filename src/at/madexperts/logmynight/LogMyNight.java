d7913a23d91dd580dbd5304b18421115db2d6a1c
Michael Greifeneder <mikegr@gmx.net>
2010-04-22 09:18:27 +0200

    Useable section with SectionAdapter

--- src/at/madexperts/logmynight/LogMyNight.java
+++ src/at/madexperts/logmynight/LogMyNight.java
@@ -1,18 +1,26 @@
 package at.madexperts.logmynight;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import android.app.Activity;
 import android.app.ListActivity;
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
-public class LogMyNight extends Activity
+public class LogMyNight extends ListActivity
 {
 	
+	private final static String TAG = LogMyNight.class.getName();
 	String[] lastItems = new String[] {"Beer"};
 	String[] favItems = new String[] {"Beer", "Wine"};
 	String[] allItems = new String[] {"Beer", "Wine", "Cocktail", "Prosecco", "Water"};
@@ -22,31 +30,101 @@ public class LogMyNight extends Activity
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.sections);
+        setContentView(R.layout.main);
         
-        ListView allList = (ListView) findViewById(R.id.allList);
-        allList.setAdapter(new GradientAdapter(this, allItems));
-        ListView favourites = (ListView) findViewById(R.id.favouriteList);
-        favourites.setAdapter(new GradientAdapter(this,favItems));
-        ListView lastItem = (ListView) findViewById(R.id.lastList);
-        lastItem.setAdapter(new GradientAdapter(this, lastItems));
+        List<Adapter> adapters = new ArrayList<Adapter>();
+        adapters.add(new GradientAdapter(this, "Last", lastItems));
+        adapters.add(new GradientAdapter(this, "Favourites", favItems));
+        adapters.add(new GradientAdapter(this, "All drinks", allItems));
+        
+        setListAdapter(new SectionAdapter(adapters));
         
         //setListAdapter(new GradientAdapter(this));
     }
     
+    class SectionAdapter extends BaseAdapter {
+    	
+    	private List<Adapter> adapters;
+    	
+    	public SectionAdapter(List<Adapter> adapters) {
+    		this.adapters = adapters;
+    	}
+    	
+    	public int getCount() {
+    		int count = 0;
+    		for (Adapter a:adapters) {
+    			count++; //header
+				count += a.getCount();
+			}
+    		Log.d(TAG, " count all sections + headers:" + count);
+    		return count;
+    	}
+    	
+    	public Object getItem(int position) {
+    		int counter = 0;
+    		for(Adapter adapter:adapters) {
+    			if (position == counter) { //header
+    				return null;  
+    			}
+    			int size = adapter.getCount();
+    			if (counter + size >= position) {
+    				return adapter.getItem(position - counter - 1);
+    			}
+    			counter = counter + size + 1;
+    		}
+    		return null;
+    	}
+    	
+    	public long getItemId(int position) {
+    		return position; //TODO: convert to item id
+    	}
+    	public View getView(int position, View convertView, ViewGroup parent) {
+    		Log.d(TAG, "Requested view position:" + position);
+    		int counter = 0;
+    		for(Adapter adapter:adapters) {
+    			if (position == counter) { //header
+    				View view = getLayoutInflater().inflate(R.layout.sectionheader, parent, false);
+    				TextView text = (TextView) view.findViewById(R.id.sectionHeader);
+    				text.setText("Some Header");
+    				return view;
+    			}
+    			int size = adapter.getCount();
+    			if (counter + size >= position) {
+    				return adapter.getView(position - counter - 1, convertView, parent);
+    			}
+    			counter = counter + size + 1;
+    		}
+    		return null;
+    	}
+    
+    	@Override
+    	public boolean isEnabled(int position) {
+    		int counter = 0;
+    		for(Adapter adapter:adapters) {
+    			if (position == counter) { //header
+    				return false;  
+    			}
+    			counter = counter + adapter.getCount() + 1;
+    		}
+    		return true; 
+    	}
+    }
     class GradientAdapter extends ArrayAdapter {
-    	public GradientAdapter(Context ctx, String[] items) {
+    	private String header;
+    	public GradientAdapter(Context ctx, String header, String[] items) {
 			super(ctx, R.layout.row, R.id.rowText, items);
 		}
     	
     	@Override
     	public View getView(int position, View convertView, ViewGroup parent) {
+    		Log.d(TAG, "Requested in Gradient " + header + ":"+ position);
     		View view = super.getView(position, convertView, parent);
     		//setBackgroundResource(R.drawable.entry);
     		view.setBackgroundResource(R.drawable.entry);
     		
     		return view;
     	}
+    	
     }
     
     
