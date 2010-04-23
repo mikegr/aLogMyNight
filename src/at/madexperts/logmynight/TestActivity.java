9f1c5920ddd89f7520a8146371e8e0628055d5eb
Michael Greifeneder <mikegr@gmx.net>
2010-04-23 16:27:40 +0200

    add log view

---
+++ src/at/madexperts/logmynight/TestActivity.java
@@ -0,0 +1,13 @@
+package at.madexperts.logmynight;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TestActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.logdrink);	
+	}
+}
