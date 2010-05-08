aedacbd709c375b395ce40dc98940a085e28af05
Oliver Selinger <os@matador.(none)>
2010-05-08 22:50:38 +0200

    landscape chart with working bar size and labels with the drink names under it

---
+++ src/at/madexperts/logmynight/chart/BarChartDummyData.java
@@ -0,0 +1,30 @@
+package at.madexperts.logmynight.chart;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BarChartDummyData {
+	
+	public static List<BarItem> getTestData() {
+		List<BarItem> items = new ArrayList<BarItem>();
+		
+		BarItem item1 = new BarItem("halbe Bier", 300);
+		BarItem item2 = new BarItem("große Bier", 200);
+		BarItem item3 = new BarItem("Mockito", 100);
+		BarItem item4 = new BarItem("Schnaps", 50);
+		BarItem item5 = new BarItem("Mineral", 20);
+		BarItem item6 = new BarItem("Red Bull", 10);
+		BarItem item7 = new BarItem("Cola Bull", 5);
+		
+		items.add(item1);
+		items.add(item2);
+		items.add(item3);
+		items.add(item4);
+		items.add(item5);
+		items.add(item6);
+		items.add(item7);
+		
+		return items;
+	}
+
+}
