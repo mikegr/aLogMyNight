e788916de845bfd7b23615d71298faf4408967d6
Oliver Selinger <oliver.selinger@gmail.com>
2010-05-14 21:52:31 +0200

    finalized gallery, beautifications of history + renamed all labels to the corresponding german word

--- src/at/madexperts/logmynight/HistoryActivity.java
+++ src/at/madexperts/logmynight/HistoryActivity.java
@@ -24,6 +24,7 @@ import com.codecarpet.fbconnect.FBFeedActivity;
 
 import android.app.Activity;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
@@ -37,11 +38,13 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
@@ -59,10 +62,11 @@ public class HistoryActivity extends Activity implements OnClickListener,
 	private ListView sumView;
 	private Button button;
 
-	private BarChartView barChartView;
-	
-    private static final int MESSAGE_PUBLISHED = 2;
+	private TextView checkLabel;
 
+	private BarChartView barChartView;
+
+	private static final int MESSAGE_PUBLISHED = 2;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -77,34 +81,39 @@ public class HistoryActivity extends Activity implements OnClickListener,
 		/* Now we can retrieve all display-related infos */
 		int orientation = display.getOrientation();
 		Log.d(TAG, "Orientation: " + orientation);
-		
+
 		if (orientation == 0) {
 			setContentView(R.layout.history);
 			listView = (ListView) findViewById(R.id.historyListView);
-			View view = getLayoutInflater().inflate(R.layout.historyrow, null);
-			((TextView) view.findViewById(R.id.historyRowName)).setText("Drink");
+			View view = getLayoutInflater().inflate(R.layout.historyheader,
+					null);
+			((TextView) view.findViewById(R.id.historyRowName))
+					.setText("Drink");
 			((TextView) view.findViewById(R.id.historyRowSum)).setText("Summe");
 			((TextView) view.findViewById(R.id.historyRowAmount))
 					.setText("Menge");
 			listView.addHeaderView(view);
 
+			checkLabel = (TextView) findViewById(R.id.checkText);
+			checkLabel.setText("letzten 24h");
+
 			sumView = (ListView) findViewById(R.id.historySum);
 			button = (Button) findViewById(R.id.historyTimeButton);
 			button.setOnClickListener(this);
 			showHistory(1);
-		} else {			
+		} else {
 			setContentView(R.layout.barchart);
-			
+
 			days = (Integer) getLastNonConfigurationInstance();
-			if(days == null)
+			if (days == null)
 				days = 1;
-			
+
 			createBarChartDataList(days);
 			barChartView = (BarChartView) findViewById(R.id.barChartView);
 			barChartView.setData(barChartData);
 		}
 	}
-	
+
 	Integer days;
 
 	@Override
@@ -117,37 +126,38 @@ public class HistoryActivity extends Activity implements OnClickListener,
 		super.onConfigurationChanged(configuration);
 		Log.d(TAG, "onConfigurationChanged");
 	}
-	
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-    	new MenuInflater(this).inflate(R.menu.historymenu, menu);
-    	return super.onCreateOptionsMenu(menu);
-    }
-    
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-    	switch (item.getItemId()) {
-    		case R.id.shareMenuItem: 
-    			share("Message from Android!");
-    			return true;
-    	}
-    	// TODO Auto-generated method stub
-    	return super.onOptionsItemSelected(item);
-    }
 
-    private void share(String txt) {
-    	FacebookHelper h = new FacebookHelper(this);
-    	h.publish(txt);
-    }
-    
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		new MenuInflater(this).inflate(R.menu.historymenu, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.shareMenuItem:
+			share("Message from Android!");
+			return true;
+		}
+		// TODO Auto-generated method stub
+		return super.onOptionsItemSelected(item);
+	}
+
+	private void share(String txt) {
+		FacebookHelper h = new FacebookHelper(this);
+		h.publish(txt);
+	}
+
 	private Dialog dlg;
 	private ListView timeView;
 
 	public void onClick(View v) {
 		Log.d(TAG, "onClick from ID: " + v.getId());
 		ArrayAdapter<String> a = new ArrayAdapter<String>(this,
-				android.R.layout.simple_list_item_1, new String[] { "letzten 24h",
-						"letzte Woche", "letzter Monat", "Alle" });
+				android.R.layout.simple_list_item_1,
+				new String[] { "letzten 24h", "letzte Woche", "letzter Monat",
+						"Alle" });
 		timeView = new ListView(this);
 		timeView.setAdapter(a);
 		timeView.setOnItemClickListener(this);
@@ -165,15 +175,19 @@ public class HistoryActivity extends Activity implements OnClickListener,
 		switch ((int) id) {
 		case 0:
 			showHistory(1);
+			checkLabel.setText("letzten 24h");
 			break;
 		case 1:
 			showHistory(7);
+			checkLabel.setText("letzte Woche");
 			break;
 		case 2:
 			showHistory(30);
+			checkLabel.setText("letzten Monat");
 			break;
 		case 3:
 			showHistoryAll();
+			checkLabel.setText("gesamter Zeitraum");
 			break;
 		default:
 		}
@@ -238,38 +252,70 @@ public class HistoryActivity extends Activity implements OnClickListener,
 
 	private void showHistory(int days) {
 		this.days = days;
-		
+
 		// SELECT d._id, d.name as name, COUNT(l._id) as counter, SUM(l.price)
 		// as itemsum FROM drinks d JOIN drinklog l ON d._id = l.drink_id WHERE
 		// l.log_time >= datetime('now', '-2 day') GROUP BY d._id, d.name;
 		Cursor cursor = db
 				.rawQuery(
-						"SELECT d._id as _id, d.name as name, COUNT(l._id) as counter, SUM(l.price) as itemsum "
+						"SELECT d._id as _id, d.name as name, d.category as category, COUNT(l._id) as counter, SUM(l.price) as itemsum "
 								+ "FROM drinks d JOIN drinklog l ON d._id = l.drink_id "
 								+ "WHERE l.log_time >= datetime('now', '-"
 								+ days + " day') " + "GROUP BY d._id, d.name",
 						null); // new String[] {Integer.toString(days)});
-		SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
-				R.layout.historyrow, cursor, new String[] { "name", "counter",
-						"itemsum" }, new int[] { R.id.historyRowName,
-						R.id.historyRowAmount, R.id.historyRowSum });
+//		SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
+//				R.layout.historyrow, cursor, new String[] { "name", "counter",
+//						"itemsum" }, new int[] { R.id.historyRowName,
+//						R.id.historyRowAmount, R.id.historyRowSum });
 
 		Cursor sumCursor = db
 				.rawQuery(
-						"SELECT '0' as _id, 'Sum:' as name, COUNT(l._id) as counter, SUM(l.price) as itemsum "
+						"SELECT '0' as _id, 'Summe:' as name, COUNT(l._id) as counter, SUM(l.price) as itemsum "
 								+ "FROM drinks d JOIN drinklog l ON d._id = l.drink_id "
 								+ "WHERE l.log_time >= datetime('now', '-"
 								+ days + " day') ", null); // new String[]
 		// {Integer.toString(days)});
 
 		SimpleCursorAdapter sumAdapter = new SimpleCursorAdapter(this,
-				R.layout.historyrow, sumCursor, new String[] { "name",
+				R.layout.historysum, sumCursor, new String[] { "name",
 						"counter", "itemsum" }, new int[] {
 						R.id.historyRowName, R.id.historyRowAmount,
 						R.id.historyRowSum });
 		sumView.setAdapter(sumAdapter);
 
-		listView.setAdapter(adapter);
+		listView.setAdapter(new ImageAdapter(this, cursor));
+	}
+
+	class ImageAdapter extends SimpleCursorAdapter {
+		private String header;
+
+		public ImageAdapter(Context ctx, Cursor cursor) {
+			super(ctx, R.layout.historyrow, cursor, new String[] { "category", "name",
+					"counter", "itemsum" }, new int[] { R.id.historyRowImage,
+					R.id.historyRowName, R.id.historyRowAmount,
+					R.id.historyRowSum });
+		}
+
+		/*
+		 * @Override
+		 * 
+		 * public void setViewImage(ImageView v, String value) { Log.d(TAG,
+		 * "setImageView: " + value); super.setViewImage(v, getIcon(value)); }
+		 */
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			Log.d(TAG, "Requested in Gradient " + header + ":" + position);
+			View view = super.getView(position, convertView, parent);
+			// setBackgroundResource(R.drawable.entry);
+			//view.setBackgroundResource(R.drawable.entry);
+			ImageView imageView = (ImageView) view.findViewById(R.id.historyRowImage);
+			Cursor cursor = getCursor();
+			int category = cursor.getInt(cursor.getColumnIndex("category"));
+			imageView.setImageResource(Utilities.getIcon(category));
+			return view;
+		}
+
 	}
 
 	@Override
