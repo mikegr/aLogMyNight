e751e2b1fc42e8380b7bc2c2dd6cfc5ab4a75b58
Michael Greifeneder <mikegr@gmx.net>
2010-05-30 11:04:33 +0200

    custom layout for tabWidget

--- src/at/madexperts/logmynight/MyTabActivity.java
+++ src/at/madexperts/logmynight/MyTabActivity.java
@@ -1,12 +1,18 @@
 package at.madexperts.logmynight;
 
 import android.app.TabActivity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TabHost;
 import android.widget.TabWidget;
+import android.widget.TextView;
 
 public class MyTabActivity extends TabActivity {
 
@@ -15,18 +21,21 @@ public class MyTabActivity extends TabActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
+	    
 	    setContentView(R.layout.tablayout);
 
 	    mTabHost = getTabHost();
 	    
 	    
-	    mTabHost.addTab(mTabHost.newTabSpec("tab_test1").setIndicator(getImageView(R.drawable.icon_bier_small)).setContent(R.id.textview1));
+	    mTabHost.addTab(mTabHost.newTabSpec("tab_test1").setIndicator(getView("Loggen", R.drawable.icon_bier_small)).setContent(R.id.textview1));
 	    
-	    mTabHost.addTab(mTabHost.newTabSpec("tab_test2").setIndicator(getImageView(R.drawable.icon_wein_small)).setContent(R.id.textview2));
-	    mTabHost.addTab(mTabHost.newTabSpec("tab_test3").setIndicator(getImageView(R.drawable.icon_anti_small)).setContent(R.id.textview3));
-	    mTabHost.addTab(mTabHost.newTabSpec("tab_test4").setIndicator(getImageView(R.drawable.icon_bier_small)).setContent(R.id.textview1));
-	    mTabHost.addTab(mTabHost.newTabSpec("tab_test5").setIndicator(getImageView(R.drawable.icon_bier_small)).setContent(R.id.textview1));
-	    mTabHost.addTab(mTabHost.newTabSpec("tab_test6").setIndicator(getImageView(R.drawable.icon_bier_small)).setContent(R.id.textview1));
+	    mTabHost.addTab(mTabHost.newTabSpec("tab_test2").setIndicator(getView("Rechnung", R.drawable.icon_wein_small)).setContent(R.id.textview2));
+	    mTabHost.addTab(mTabHost.newTabSpec("tab_test3").setIndicator(getView("Aufrisse", R.drawable.icon_anti_small)).setContent(R.id.textview3));
+	    mTabHost.addTab(mTabHost.newTabSpec("tab_test4").setIndicator(getView("Lokale", R.drawable.icon_bier_small)).setContent(R.id.textview1));
+	    
+	    /*mTabHost.addTab(mTabHost.newTabSpec("tab_test5").setIndicator(getView(R.drawable.icon_bier_small)).setContent(R.id.textview1));
+	      mTabHost.addTab(mTabHost.newTabSpec("tab_test6").setIndicator(getView(R.drawable.icon_bier_small)).setContent(R.id.textview1));
+	    */
 	    
 	    TabWidget tabWidget = getTabWidget();
 	    for(int i=0;i < tabWidget.getChildCount();i++) {
@@ -36,9 +45,15 @@ public class MyTabActivity extends TabActivity {
 	    mTabHost.setCurrentTab(0);
 	}
 	
-	ImageView getImageView(int id) {
-		ImageView image = new ImageView(this);
-		image.setBackgroundDrawable(getResources().getDrawable(id));
-		return image;
+	
+	View getView(String name, int imageId) {
+		View tab = getLayoutInflater().inflate(R.layout.tab, null);
+		ImageView image = (ImageView) tab.findViewById(R.id.tabImage);
+		Drawable d = getResources().getDrawable(imageId);
+		d.setBounds(0, 0, d.getIntrinsicWidth(), d.getIntrinsicHeight());
+		image.setImageDrawable(d);
+		TextView text = (TextView) tab.findViewById(R.id.tabText);
+		text.setText("Drinks");
+		return tab;
 	}
 }
